52b4007a4330f3cdfca09218f16e28ec169d7f4f
Hack Reactor Student <student@hackreactor.com>
2018-04-30 20:13:39 -0700

    hi

--- src/init.js
+++ src/init.js
@@ -16,18 +16,53 @@ $(document).ready(function() {
      * to the stage.
      */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    console.log(this)
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction (
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+    
     $('body').append(dancer.$node);
+    window.dancers.push(dancer);
+  });
+
+  $('.addJumpyButton').on('click', function(event) {
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    console.log(this)
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    var dancer = new dancerMakerFunction (
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 10000
+    );
+    
+    $('body').append(dancer.$node);
+    window.dancers.push(dancer);
+  });
+
+  $('.LineUpButton').on('click', function(event) {
+    var top = 50;
+    for(var i = 0; i < window.dancers.length; i++) {
+      window.dancers[i].setPosition(top, 500);
+      top += 30;
+    }
+  });
+
+  $('.GoBack').on('click', function(event) {
+    for(var i = 0; i < window.dancers.length; i++) {
+      window.dancers[i].setPosition(
+        $("body").height() * Math.random(),
+        $("body").width() * Math.random(),
+        Math.random() * 500
+      );
+    }
   });
 });
-
